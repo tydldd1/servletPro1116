ad6f8833cf9ea8941edec2e76214701f919b60f9
tydldd <ru2013_1@163.com>
2013-11-17 11:30:13 +0800

    log4j
    log4j

---
+++ servlet16/src/com/ru/project/utils/Test.java
@@ -0,0 +1,19 @@
+package com.ru.project.utils;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Description:
+ * User: NanChengRu
+ * Date: 13-11-17
+ * Time: 上午12:30
+ * JDK: 1.6
+ * version: 1.0
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("11111");
+        Logger log = Logger.getLogger(Test.class);
+        log.info("2222222");
+    }
+}
---
+++ servlet16/src/com/ru/project/utils/Test2.java
@@ -0,0 +1,13 @@
+package com.ru.project.utils;
+
+/**
+ * Description:
+ * User: NanChengRu
+ * Date: 13-11-17
+ * Time: 上午11:29
+ * JDK: 1.6
+ * version: 1.0
+ */
+public class Test2 {
+
+}
